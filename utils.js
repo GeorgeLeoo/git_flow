e41bba2cb015b484bb4a74272a8def3913c5cfba
GeorgeLeeo <georgeleeo@163.com>
2020-02-07 22:21:14 +0800

    update method timeAgo

--- utils.js
+++ utils.js
@@ -5,3 +5,14 @@ export const getTimeStamp = function(date) {
 export const uppercaseFirst = function(string) {
     return string.charAt(0).toUpperCase() + string.slice(1)
 }
+
+export function timeAgo(time) {
+    const between = Date.now() / 1000 - Number(time)
+    if (between < 3600) {
+      return pluralize(~~(between / 60), ' minute')
+    } else if (between < 86400) {
+      return pluralize(~~(between / 3600), ' hour')
+    } else {
+      return pluralize(~~(between / 86400), ' day')
+    }
+  }
