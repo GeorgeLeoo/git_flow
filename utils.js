68d4467fc80d425a57ee888227288dda6fceda56
GeorgeLeeo <georgeleeo@163.com>
2020-02-07 22:19:59 +0800

    add method uppercaseFirst

--- utils.js
+++ utils.js
@@ -1,3 +1,7 @@
 export const getTimeStamp = function(date) {
     return new Date(date).getTime()
 }
+
+export const uppercaseFirst = function(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+}
